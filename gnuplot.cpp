88172bc8754a759e69e2e3f068dacefe933a8311
EC2 Default User <ec2-user@ip-172-31-87-92.ec2.internal>
2022-12-11 22:46:09 +0000

    Adding SetDates

--- gnuplot.cpp
+++ gnuplot.cpp
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <algorithm>
 namespace fre{
+    
+    vector<string> GenerateDates()
+    {
+    	set<int> day31({1,3,5,7,8,10,12});
+    	set<int> day30({4,6,9,11});
+    	
+    	vector<string> DateList;
+    	
+    	int days;
+    	
+    	for (int i = 1; i < 13; i++)
+    	{
+    		if(day31.find(i) != day31.end())
+    			days = 31;
+    		else if(day30.find(i) != day30.end())
+    			days = 30;
+    		else
+    			days = 28;
+    		
+    		for(int j = 1; j < days+1; j++)
+    		{
+    			string month;
+    			string date;
+    			
+    			if (i < 10)
+    				month = "0"	+ to_string(i);
+    			else
+    				month = to_string(i);
+    			
+    			if (j < 10)
+    				date = "0" + to_string(j);
+    			else
+    				date = to_string(j);
+    			
+    			string temp_date = "2022-" + month + "-" + date;
+    			
+    			DateList.push_back(temp_date);
+    		}
+    	}
+    	return DateList;
+    }
+
+    void Stock::SetDates(vector<string> dateslist)
+	{
+		auto itr = find(dateslist.begin(), dateslist.end(), earnings_date);
+
+		int index = std::distance(dateslist.begin(), itr);
+		//cout << index + N - (index - (N + 1))<<endl ;
+		start_date = dateslist[index - N];
+		end_date = dateslist[(index + N)];
+	}
 
     void plotResults(double* xData, double* Beat, double* Meet, double* Miss, int dataSize, const char* title, const char* yLabel) {
        
