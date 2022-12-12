22fe7cdec31ffe479d4c033c6d155ea39047235f
raktim_roychoudhury <rr4081@nyu.edu>
2022-12-12 18:36:27 +0000

    raktim-roychoudhury Group Class

--- group-prototype_Lune.h
+++ group-prototype_Lune.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <map>
+#include <cmath>
+#include <algorithm>
 #include "Stocks.h"
 using namespace std;
 
@@ -13,33 +16,30 @@ class Group
     protected:
         Table Group_Mapping; //table of tickers
         int N_Group;
-        map<string,Stocks>* dataPtr;
-        
+        map<string,Stocks>* stockMapPtr;
         Group(map<string,Stocks>* data_);
         virtual ~Group() = 0;
         
     public:
-        virtual void SetGroup(int n) = 0;
-        //string GetGroup(const string& ticker) const; //Do we need this??
+        void SetN(int n);
         int GetN() const;
+        
+        map<string,Stocks>* GetStockMap() const
+        void UpdateStockMap(map<string, Stocks>* stockMapPtr_);
+        
+        virtual void CreateGroups = 0;
+        virtual void CreateGroups(int n) = 0;
+        virtual void CreateGroups(map<string, Stocks>* stockMapPtr_, int n) = 0;
 };
 
 class Groupby_Surprise: public Group
 {
+    protected:
+        bool cmp_bysurprise(pair<string, Stocks>& a, pair<string, Stocks>& b) const;
     public:
-        Groupby_Surprise(map<string,Stocks>* dataPtr_);
+        Groupby_Surprise(map<string,Stocks>* stockMapPtr_);
         ~Groupby_Surprise();
-        void SetGroup(int n);
-        map<string,Stocks>* GetdataPtr() const;
-}
-
-Group::Group(map<string,Stocks>* dataPtr_): dataPtr(dataPtr_) {}
-
-Groupby_Surprise::Groupby_Surprise(map<string,Stocks>* dataPtr_): Group(dataPtr_) {}
-Groupby_Surprise::~Groupby_Surprise(){}
-void Groupby_Surprise::SetGroup(int n)
-{
-    N_Group = n;
-    Group_Mapping.resize(N_Group);
-    //Store Tickers in Matrix
+        void CreateGroups();
+        void CreateGroups(int n);
+        void CreateGroups(map<string,Stocks>* stockMapPtr_, int n);
 }
