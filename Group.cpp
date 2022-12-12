66c4f976290fb8fef1d74476ad79b40049133d11
raktim_roychoudhury <rr4081@nyu.edu>
2022-12-12 19:05:39 +0000

    rrc more Group updates

--- Group.cpp
+++ Group.cpp
@@ -30,6 +30,31 @@ map<string, Stocks>* Group::GetStockMap() const{
     return stockMapPtr;
 }
 
+//Create groups
+void Group::CreateGroups(){
+    vector<Stocks> stockVector;
+    for (auto it = stockMapPtr->begin(); it != stockMapPtr.end(); ++it) stockVector.push_back(it->second);
+    sort(stockVector.begin(), stockVector.end(), compare, reverse = true);
+    
+    int NoOfStocks = (int)(stockVector.size()/N_group);
+    
+    for (int i = 0; i < N_group; ++i){
+        for (int j = i * NoOfStocks; j < min(i + NoOfStocks, stockVector.size()); ++j){
+            Group_Mapping[i].push_back(stockVector[j].GetTicker());
+        }
+    }
+}
+
+void Group::CreateGroups(int n){
+    SetN(n);
+    CreateGroups();
+}
+
+void Group::CreateGroups(map<string, Stocks>* stockMapPtr_, int n){
+    UpdateStockMap(stockMapPtr_);
+    CreateGroups(n);
+}
+
 //Groupby_Surprise derived class
 
 
@@ -41,31 +66,10 @@ Groupby_Surprise::~Groupby_Surprise(){
 
 //to be completed
 
-bool Groupby_surprise::cmp_bysurprise(Stocks& a, Stocks& b) const{
+bool Groupby_surprise::compare(const Stocks& a, const Stocks& b) const{
     return (a.GetSurprisePerecent() < b.GetSurprisePerecent());
 }
 
-void Groupby_Surprise::CreateGroups(){
-    vector<Stocks> stockVector;
-    for (auto it = stockMapPtr->begin(); it != stockMapPtr.end(); ++it) stockVector.push_back(it->second);
-    sort(stockVector.begin(), stockVector.end(), cmp_bysurprise, reverse = true);
-    
-    int NoOfStocks = (int)(stockVector.size()/N_group);
-    
-    for (int i = 0; i < N_group; ++i){
-        for (int j = i * NoOfStocks; j < min(i + NoOfStocks, stockVector.size()); ++j){
-            Group_Mapping[i].push_back(stockVector[j].GetTicker());
-        }
-    }
-}
 
 
-void Groupby_Surprise::CreateGroups(int n){
-    SetN(n);
-    CreateGroups();
-}
 
-void Groupby_Surprise::CreateGroups(map<string, Stocks>* stockMapPtr_, int n){
-    UpdateStockMap(stockMapPtr_);
-    CreateGroups(n);
-}
--- Group.h
+++ Group.h
@@ -14,10 +14,15 @@ typedef vector<vector<string>> Table;
 class Group
 {
     protected:
+        //data members
         Table Group_Mapping; //table of tickers
-        int N_Group;
         map<string,Stocks>* stockMapPtr;
+        int N_Group;
+        
+        //constructor
         Group(map<string,Stocks>* data_);
+        
+        virtual bool compare(const Stocks& a,const Stocks& b) const = 0;
         virtual ~Group() = 0;
         
     public:
@@ -27,19 +32,18 @@ class Group
         map<string,Stocks>* GetStockMap() const
         void UpdateStockMap(map<string, Stocks>* stockMapPtr_);
         
-        virtual void CreateGroups = 0;
-        virtual void CreateGroups(int n) = 0;
-        virtual void CreateGroups(map<string, Stocks>* stockMapPtr_, int n) = 0;
+        void CreateGroups;
+        void CreateGroups(int n);
+        void CreateGroups(map<string, Stocks>* stockMapPtr_, int n);
 };
 
 class Groupby_Surprise: public Group
 {
+    //comparator to sort stocks by attribute
     protected:
-        bool cmp_bysurprise(pair<string, Stocks>& a, pair<string, Stocks>& b) const;
+        bool compare(const Stocks& a,const Stocks& b) const;
+        
     public:
         Groupby_Surprise(map<string,Stocks>* stockMapPtr_);
         ~Groupby_Surprise();
-        void CreateGroups();
-        void CreateGroups(int n);
-        void CreateGroups(map<string,Stocks>* stockMapPtr_, int n);
 }
